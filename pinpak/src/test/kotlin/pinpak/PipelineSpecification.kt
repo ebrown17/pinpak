a7ecb831b9d150df597fca2e96f3c27c14654276
ebrown <ebrown@bart.gov>
2021-10-16 13:12:30 -0700

    finished first pipeline interceptor test

--- pinpak/src/test/kotlin/pinpak/PipelineSpecification.kt
+++ pinpak/src/test/kotlin/pinpak/PipelineSpecification.kt
@@ -3,40 +3,46 @@ package pinpak
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.data.forAll
 import io.kotest.data.row
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
-import pinpak.core.PinPak
+import pinpak.core.*
 
 class PipelineInterceptorTest  : StringSpec({
     val logger = logger(PipelineInterceptorTest::class.java)
 
     "Pipeline has expected amount of Interceptors using addLast" {
         forAll(
-            row(50, arrayOf()),
-            row(50, arrayOf("1")),
-            row(50, arrayOf("1","2")),
-            row(100, arrayOf("1","2","3")),
-            row(500, arrayOf("1","2","3","4")),
-            row(500, arrayOf("1","2","3","4","5","6","7")),
-        ) { totalMessages, interceptorNames ->
+            row(0, arrayOf(), arrayOf()),
+            row(1, arrayOf("1"), arrayOf("1")),
+            row(2, arrayOf("1","2"), arrayOf("1","2")),
+            row(1, arrayOf("1","1"), arrayOf("1")),
+            row(1, arrayOf("1","1","1"), arrayOf("1")),
+            row(2, arrayOf("1","1","2","2"), arrayOf("1","2")),
+            row(2, arrayOf("1","PipelineInterceptorTest-HeadContext","2"), arrayOf("1","2")),
+            row(2, arrayOf("1","PipelineInterceptorTest-TailContext","2"), arrayOf("1","2")),
+            row(2, arrayOf("1","PipelineInterceptorTest-HeadContext","PipelineInterceptorTest-TailContext","2"), arrayOf("1","2")),
+            row(7, arrayOf("1","2","3","4","5","6","7"), arrayOf("1","2","3","4","5","6","7")),
+        ) {total, interceptorNames, expectedNames ->
 
-            val integerPipeline = PinPak.create("InterceptorAddTest") { config ->
-                interceptorNames.forEach { name ->
-                    config.addInterceptorLast(name, PassThroughIntegerInterceptorTest())
-                }
-            }
+            val stringPipeline = PinPak.create("PipelineInterceptorTest")
 
-            for( i in 1..totalMessages){
-                integerPipeline.injectData(1)
-            }
 
-            var received = 0
             interceptorNames.forEach { name ->
-                val interceptor =  integerPipeline.getInterceptor(name) as PassThroughIntegerInterceptorTest
-                received += interceptor.totalReceived
+               stringPipeline.pipeline.addLast(name, PassThroughStringInterceptor())
             }
 
-            logger.info("shouldBe $totalMessages * ${interceptorNames.size}")
-            received shouldBe totalMessages * interceptorNames.size
+
+            logger.info("pipeline size: ${stringPipeline.pipeline.getSize()} shouldBe $total")
+            stringPipeline.pipeline.getSize() shouldBe total
+
+            var context = stringPipeline.pipeline.getFirst()
+
+            var count = 0
+            while(context != stringPipeline.pipeline.getTail()){
+                logger.info("context name: ${context.name} shouldBe ${expectedNames[count]}")
+                context.name shouldBe expectedNames[count++]
+                context = context.next
+            }
         }
     }
 })
